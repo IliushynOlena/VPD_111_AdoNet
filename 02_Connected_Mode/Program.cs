4ec44ee29b59514275cddd7045fcaed2db11d3c7
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-03-05 17:57:48 +0200

    Sql to Linq

--- 02_Connected_Mode/Program.cs
+++ 02_Connected_Mode/Program.cs
@@ -1,114 +1,12 @@
-﻿using System;
+﻿using _04_data_access;
+using _04_data_access.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
 
 namespace _02_Connected_Mode
 {
-    class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public int Quantity { get; set; }
-        public int CostPrice { get; set; }
-        public string Producer { get; set; }
-        public int Price { get; set; }
-    }
-    class SportShopDb
-    {
-        //CRUD
-        //[C]reate
-        //[R]ead
-        //[U]pdate
-        //[D]elete
-        private SqlConnection connection;
-      
-        public SportShopDb(string connectionString)
-        {
-            connection = new SqlConnection(connectionString);
-            connection.Open();
-        }
-        ~SportShopDb()
-        {
-            connection.Close();
-        }
-        public void Create(Product product)
-        {
-            string cmdText = $@"INSERT INTO Products
-                              VALUES ('{product.Name}', 
-                                      '{product.Type}', 
-                                       {product.Quantity}, 
-                                       {product.CostPrice}, 
-                                      '{product.Producer}',
-                                       {product.Price})";
-            SqlCommand command = new SqlCommand(cmdText, connection);
-            command.ExecuteNonQuery();
-        }
-        public List<Product> GetAll()
-        {
-            string cmdText = @"select * from Products";
-            SqlCommand command = new SqlCommand(cmdText, connection);
-            SqlDataReader reader = command.ExecuteReader();
-            List<Product> products = new List<Product>();
-            while (reader.Read())
-            {
-                products.Add(new Product()
-                {
-                    Id = (int)reader[0],
-                    Name = (string)reader[1],
-                    Type = (string)reader[2],
-                    Quantity = (int)reader[3],
-                    CostPrice = (int)reader[4],
-                    Producer = (string)reader[5],
-                    Price = (int)reader[6]
-                });
-            }
-            reader.Close();
-            return products;
-        }
-        public void Update(Product product)
-        {
-            string cmdText = $@"UPDATE Products
-                              SET Name = '{product.Name}', 
-                                  TypeProduct = '{product.Type}', 
-                                  Quantity = {product.Quantity}, 
-                                   CostPrice =  {product.CostPrice}, 
-                                   Producer = '{product.Producer}',
-                                    Price =  {product.Price}
-                                WHERE Id = {product.Id}";
-            SqlCommand command = new SqlCommand(cmdText, connection);
-            command.ExecuteNonQuery();
-        }
-        public void Delete(int id)
-        {
-            string cmdText = $@"delete  Products WHERE Id = {id}";
-            SqlCommand command = new SqlCommand(cmdText, connection);
-            command.ExecuteNonQuery();
-        }
-        public Product GetOneById(int id)
-        {
-            string cmdText = $@"select * from Products where Id = {id}";
-            SqlCommand command = new SqlCommand(cmdText, connection);
-            SqlDataReader reader = command.ExecuteReader();
-            Product product = new Product();
-            while (reader.Read())
-            {
-
-                product.Id = (int)reader[0];
-                product.Name = (string)reader[1];
-                product.Type = (string)reader[2];
-                product.Quantity = (int)reader[3];
-                product.CostPrice = (int)reader[4];
-                product.Producer = (string)reader[5];
-                product.Price = (int)reader[6];
-               
-            }
-            reader.Close();
-            return product;
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
@@ -127,6 +25,15 @@ namespace _02_Connected_Mode
                 Producer = "China",
                 Price = 1200
             };
+
+            Console.WriteLine("Enter namr of product : ");
+            string name = Console.ReadLine();
+            List<Product> products =  db.GetByName(name);
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " +
+                    item.Producer + " " + item.Price);
+            }
             //db.Create(product);
             //var prod = db.GetAll();
             //foreach (var item in prod)
@@ -138,9 +45,9 @@ namespace _02_Connected_Mode
             //p.Price += 500;
             //p.CostPrice += 400;
             //db.Update(p);
-            db.Delete(19);
+            // db.Delete(19);
 
-           
+
         }
     }
 }
---
+++ 06_LinqToSql/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_LinqToSql
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ShopClassesDataContext context = new ShopClassesDataContext();
+            //CRUD - create update delete read
+            //var products = context.Products;
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine($"Product : {p.Id}  {p.Name}  {p.Price}");
+            //}
+
+            //var products = context.Products
+            //    .Where(p => p.Price > 300)
+            //    .OrderByDescending(p=>p.Price)
+            //    .Take(5);
+
+            //var products = (from p in context.Products
+            //               where p.Price > 300
+            //               orderby p.Price descending                         
+            //               select p).Take(5);
+
+
+
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine($"Product : {p.Id,-10}{p.Name,-25}{p.Price,-20}");
+            //}
+
+            var product = new Product()
+            {
+                Name = "Tenis ball",
+                Price = 30,
+                Producer = "China",
+                CostPrice = 5,
+                Quantity = 10,
+                TypeProduct = "Sport Equipment"
+            };
+
+            //context.Products.InsertOnSubmit(product);
+            //context.Products.InsertOnSubmit(new Product() { });
+
+            //context.SubmitChanges();
+
+            //var productToEdit = context.Products.Where(p => p.Id == 4).FirstOrDefault();
+            //var productToEdit = context.Products.FirstOrDefault(p => p.Id == 4);
+            //productToEdit.Price += 500;
+
+            //context.SubmitChanges();
+
+            var productToDelete = context.Products.FirstOrDefault(p => p.Id == 23);
+
+            if (productToDelete != null)
+                context.Products.DeleteOnSubmit(productToDelete);
+
+            context.SubmitChanges();
+            var products = context.Products;
+            foreach (var p in products)
+            {
+                Console.WriteLine($"Product : {p.Id}  {p.Name}  {p.Price}");
+            }
+
+
+
+        }
+    }
+}
